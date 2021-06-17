3f0d18b1ce4edfa4abb00fac1df7c1c79a7b1adc
karpginex <karpginex@yandex.ru>
2021-06-18 04:48:37 +0500

    finished intro

--- index.php
+++ index.php
@@ -1,14 +1,38 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <link href="source/css/style.css" rel="stylesheet">
+      <link rel="preconnect" href="https://fonts.gstatic.com">
+      <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
+      <title>Pyrokinesis</title>
+</head>
 <body>
 
-<h1>HTML Документ</h1>
-
-<div class="image_bg">
-      <span style="background-image: url('source/images/background.jpg');"></span>
+<div class="header">
+      <div class="container">
+            <div class="header__inner">
+                  <div class="header__logo">KARPGINEX</div>
+                  <nav class="nav">
+                        <a class="nav__link" href="#">About</a>
+                        <a class="nav__link" href="#">News</a>
+                        <a class="nav__link" href="#">Blog</a>
+                        <a class="nav__link" href="#">Tour</a>
+                        <a class="nav__link" href="#">Contact</a>
+                  </nav>
+            </div>
+      </div>
 </div>
 
-<link rel="stylesheet" type="text/css" href="source/styles/main.css">
+<div class="intro">
+      <div class="container">
+            <div class="intro__inner">
+            <h1 class="intro__title">Welcome to pyro</h1>
+            <a class="button" href="#">Learn More</a>
+
+            </div>
+      </div>
+</div>
 
 </body>
 </html>
