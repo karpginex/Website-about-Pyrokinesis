150bd62a3695bd80e90fed84100948aa06deffe5
karpginex <karpginex@yandex.ru>
2021-06-17 23:58:52 +0500

    just started my site

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>HTML Документ</h1>
+
+<div class="image_bg">
+      <span style="background-image: url('source/images/background.jpg');"></span>
+</div>
+
+<link rel="stylesheet" type="text/css" href="source/styles/main.css">
+
+</body>
+</html>
