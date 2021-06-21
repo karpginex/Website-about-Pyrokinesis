71ee3f6b701f98ca516b4ce06be353e1560fec12
karpginex <karpginex@yandex.ru>
2021-06-21 06:19:24 +0500

    added about page

--- index.php
+++ index.php
@@ -41,14 +41,56 @@
 </div>
 </div>
 
+<div class="indent">
+
+</div>
+
 <section class="section">
       <div class="container">
             <div class="section__header">
-                  <h2 class="section__title">Pyrokinesis</h2>
+                  <h3 class="section__suptitle">pyrokinesis</h3>
+                  <h2 class="section__title">About us</h2>
+
                   <div class="section__text">
-                        <p>Some text</p>
+                        <p>This is a site about a modern artist. Here you can find information about the concert tour, contact information, as well as all the news.</p>
                   </div>
             </div>
+
+            <div class="about">
+                  <div class="about__item">
+                        <div class="about__img">
+                              <img src="source/images/about/musician.jpg" alt="">
+                        </div>
+                        <div class="about__text">
+                              musician
+                        </div>
+                  </div>
+                  <div class="about__item">
+                        <div class="about__img">
+                              <img src="source/images/about/poet.jpg" alt="">
+                        </div>
+                        <div class="about__text">
+                              poet
+                        </div>
+                        
+                  </div>
+                  <div class="about__item">
+                        <div class="about__img">
+                              <img src="source/images/about/pikachu.jpg" alt="">
+                        </div>
+                        <div class="about__text">
+                              pikachu
+                        </div>
+
+                  </div>
+
+            </div>
+            <div class="just__button">
+                  <a class="button__item" href="#">
+                              tour
+                  </a>
+             </div>
+                  
       </div>
 </section>
 
