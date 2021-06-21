bcd2f4d0beb41e52b4015ddfb2fad8449f9f60ba
karpginex <karpginex@yandex.ru>
2021-06-21 06:57:48 +0500

    added animation

--- source/scripts/main.js
+++ source/scripts/main.js
@@ -15,3 +15,31 @@ setInterval(() => {
     updateConcertClock();
 }, 1000);
 
+
+/* Animation with scrolling */
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+        change.target.classList.add('element__show');
+      }
+    });
+  }
+
+
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.section__header');
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+
+  let elements__about = document.querySelectorAll('.about');
+  for (let elm of elements__about) {
+    observer.observe(elm);
+  }
+
+  let elements__title = document.querySelectorAll('.intro__title');
+  for (let elm of elements__title) {
+    observer.observe(elm);
+  }
